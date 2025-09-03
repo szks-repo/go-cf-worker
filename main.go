ad1c2d5b34be9cb139ee01f4aeeea037036747dc
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-09-03 20:43:08 +0900

    tidy: style

--- main.go
+++ main.go
@@ -11,16 +11,18 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		msg := "Hello!"
 		w.Write([]byte(msg))
 	})
-	http.HandleFunc("/echo", func(w http.ResponseWriter, req *http.Request) {
-		b, err := io.ReadAll(req.Body)
+
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		b, err := io.ReadAll(r.Body)
 		if err != nil {
 			panic(err)
 		}
 		io.Copy(w, bytes.NewReader(b))
 	})
+
 	workers.Serve(nil) // use http.DefaultServeMux
 }
